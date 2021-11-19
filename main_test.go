46932b4c67024400ac0d14c51865702a7601a69b
RyuheiKawabata <kawabataryuhei@gmail.com>
2021-11-20 01:06:13 +0900

    🧪 Place()についての失敗するテストを追加

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 type isValidPlaceTestCase struct {
 	x      int
@@ -25,6 +27,78 @@ func TestIsValidMove(t *testing.T) {
 		actual := isValidPlace(test.x, test.y)
 		if actual != test.expect {
 			t.Fatalf("isValidMove(%d, %d) expects %t, but got %t\n", test.x, test.y, test.expect, actual)
+		}
+	}
+}
+
+type PlaceTestCase struct {
+	board  [][]string
+	x      int
+	y      int
+	player int
+	expect [][]string
+}
+
+var placeTests = []PlaceTestCase{
+	{
+		[][]string{{"  ", "  ", "  "}, {"  ", "  ", "  "}, {"  ", "  ", "  "}},
+		1, 1, FirstPlayer,
+		[][]string{{" X", "  ", "  "}, {"  ", "  ", "  "}, {"  ", "  ", "  "}},
+	},
+	{
+		[][]string{{"  ", "  ", "  "}, {"  ", "  ", "  "}, {"  ", "  ", "  "}},
+		1, 1, SecondPlayer,
+		[][]string{{" O", "  ", "  "}, {"  ", "  ", "  "}, {"  ", "  ", "  "}},
+	},
+	{
+		[][]string{{"  ", "  ", "  "}, {"  ", "  ", "  "}, {"  ", "  ", "  "}},
+		2, 1, FirstPlayer,
+		[][]string{{"  ", "  ", "  "}, {" X", "  ", "  "}, {"  ", "  ", "  "}},
+	},
+	{
+		[][]string{{"  ", "  ", "  "}, {"  ", "  ", "  "}, {"  ", "  ", "  "}},
+		1, 2, FirstPlayer,
+		[][]string{{"  ", " X", "  "}, {"  ", "  ", "  "}, {"  ", "  ", "  "}},
+	},
+	{
+		[][]string{{"  ", "  ", "  "}, {"  ", "  ", "  "}, {"  ", "  ", "  "}},
+		3, 3, FirstPlayer,
+		[][]string{{"  ", "  ", "  "}, {"  ", "  ", "  "}, {"  ", "  ", " X"}},
+	},
+	{
+		[][]string{{"  ", "  ", "  "}, {" O", " O", " O"}, {"  ", "  ", "  "}},
+		2, 1, FirstPlayer,
+		[][]string{{"  ", "  ", "  "}, {" X", " O", " O"}, {"  ", "  ", "  "}},
+	},
+	{
+		[][]string{{"  ", "  ", "  "}, {" O", " O", " O"}, {"  ", "  ", "  "}},
+		1, 2, FirstPlayer,
+		[][]string{{"  ", " X", "  "}, {" O", " O", " O"}, {"  ", "  ", "  "}},
+	},
+}
+
+func TestPlace(t *testing.T) {
+	for _, test := range placeTests {
+		actual := Place(test.board, test.x, test.y, test.player)
+
+		if len(actual) != 3 {
+			t.Fatalf("len(actual) should be 3, but actual %d\n", len(actual))
+		}
+		for i, row := range actual {
+			if len(row) != 3 {
+				t.Fatalf("len(actual[%d]) should be 3, but actual %d\n", i, len(row))
+			}
+			if i >= 3 {
+				t.Fatalf("The board Place() returns has board[3] row.")
+			}
+		}
+
+		for y := 0; y < 3; y++ {
+			for x := 0; x < 3; x++ {
+				if actual[y][x] != test.expect[y][x] {
+					t.Fatalf("board[%d][%d] should be %s, but actual %s", y, x, test.expect[y][x], actual[y][x])
+				}
+			}
 		}
 	}
 }
